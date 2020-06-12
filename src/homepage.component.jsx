36603b2fcc0b21221783808c1cf898a322cde58c
tegar97 <eategar@gmail.com>
2020-06-12 15:09:36 +0700

    create react js and create homepage component

---
+++ src/homepage.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import './homepage.styles.scss'
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="contet">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="contet">
+                    <h1 className="title">Jackets</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="contet">
+                    <h1 className="title">Sneakers</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="contet">
+                    <h1 className="title">Womens</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="contet">
+                    <h1 className="title">MANS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+
+export default HomePage
