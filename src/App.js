fcba1d4c1fb4f108f99bfb33a95ba326ee34aabf
tegar97 <eategar@gmail.com>
2020-06-13 18:42:11 +0700

    styling shop page and collection item

--- src/App.js
+++ src/App.js
@@ -6,20 +6,16 @@ import './App.css';
 
 
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
 
-const HatsPage = () => (
-  <div>
-    <h1>Woi</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
     <Switch>
        <Route exact path='/' component={HomePage} />
-       <Route  path='/shop/hats' component={HatsPage} />
+       <Route  path='/shop' component={ShopPage} />
     </Switch>
    
     </div>
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import SHOP_DATA from './shop.data'
+import CollectionPreview from './../collection_Preview/collection-preview'
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            collection : SHOP_DATA
+        }
+    }
+    render() {
+        const {collection} = this.state
+        return(
+            <div className="shop-page">
+                {
+                    collection.map(({id, ...otherCollectionProps}) => (
+                        <CollectionPreview  key={id} {...otherCollectionProps} />
+
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default ShopPage
