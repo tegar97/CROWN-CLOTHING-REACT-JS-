d21ea1b106f0653e2e60bf523623a72db38c3e47
tegar97 <eategar@gmail.com>
2020-06-13 10:07:42 +0700

    add react-router-dom and start build shop page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {Switch,Route} from 'react-router-dom'
+
+//css
 import './App.css';
+
 
 import HomePage from './pages/homepage/homepage.component'
 
+
+const HatsPage = () => (
+  <div>
+    <h1>Woi</h1>
+  </div>
+)
+
 function App() {
-  return <div > <HomePage/> </div>
+  return (
+    <div>
+    <Switch>
+       <Route exact path='/' component={HomePage} />
+       <Route  path='/shop/hats' component={HatsPage} />
+    </Switch>
+   
+    </div>
+  )
  
 }
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-
+import {withRouter} from 'react-router-dom'
 
 import './menu-item.styles.scss'
 
-const menuItem = ({title,imageUrl,size}) =>  (
-    <div  className={`${size} menu-item`}>
+const menuItem = ({title,imageUrl,size,history,linkUrl,match}) =>  (
+    <div  className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
       <div className="background-image" style={{backgroundImage : `url(${imageUrl})`}} />
             <div className="content">
                 <h1 className="title">{title.toUpperCase()}</h1>
-                <h1 className="subtitle">Shop Now</h1>
+                <span className="subtitle">Shop Now</span>
             </div>
+            {console.log(match.url)}
  
     </div>
 
 )
 
 
-export default menuItem
+export default withRouter(menuItem)
