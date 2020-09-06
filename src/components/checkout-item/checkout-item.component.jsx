aceb79b1629508510f8525eef8dc7b3088ac3cac
Muhammad Tegar Akmal <eategar@gmail.com>
2020-09-06 21:02:58 +0700

    convert checkout-item css to css in js

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import {connect} from  'react-redux'
 import {clearItemFromCart,addItem,removeItem} from './../../redux/cart/cart.action'
-import './checkout-item.styles.scss';
-
+import {
+    CheckoutItemContainer,
+    ImageContainer,
+    TextContainer,
+    QuantityContainer,
+    RemoveButtonContainer
+  } from './checkout-item.styles';
 
 const CheckoutItem = ({CartItem,clearItem,addItem,removeItem}) => {
     const {name,imageUrl,price,quantity} = CartItem
     return (
-        <div className="checkout-item">
-            <div className="image-container">
-                <img alt="item" src={imageUrl}/>
-            </div>
-            <span className="name">{name}</span>
-            <span className="quantity">
-            <div className="arrow" onClick={() => removeItem(CartItem)}>&#10094;</div>
-                <span className="value">{quantity}</span>
-            <div className="arrow"  onClick={() => addItem(CartItem)}>&#10095;</div>
-            </span>
-            <span className="price">{price}</span>
-            <span className="remove-button" onClick={()=> clearItem(CartItem)}>&#10005;</span>
-        </div>
+        <CheckoutItemContainer>
+        <ImageContainer>
+          <img src={imageUrl} alt='item' />
+        </ImageContainer>
+        <TextContainer>{name}</TextContainer>
+        <QuantityContainer>
+          <div onClick={() => removeItem(CartItem)}>&#10094;</div>
+          <span>{quantity}</span>
+          <div onClick={() => addItem(CartItem)}>&#10095;</div>
+        </QuantityContainer>
+        <TextContainer>{price}</TextContainer>
+        <RemoveButtonContainer onClick={() => clearItem(CartItem)}>
+          &#10005;
+        </RemoveButtonContainer>
+      </CheckoutItemContainer>
 
     )
 }
